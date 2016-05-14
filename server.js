3ee07fd39a435cbd73ed15f2863fc88bf96f306b
Holman Gao <holman@golmansax.com>
2016-05-14 13:56:03 -0700

    Make port dynamic

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ server.get('/kern', (req, res) => {
     });
 });
 
-server.listen(3000, function () {
-  console.log("Listening on port 3000");
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+  console.log(`Listening on port ${port}`);
 });
