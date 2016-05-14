cd9e553cb944f3c87d4caed5b04cd535a9bf4297
Jefferson Teng <jteng3@gmail.com>
2016-05-14 15:32:38 -0700

    Refresh kernDB.js

--- scripts/kernDB.js
+++ scripts/kernDB.js
@@ -1,6 +1,7 @@
 //lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 var Promise = require('bluebird');
+var getPidToLatLongList = require('./one-off/map_pid_to_lat_long.js');
 
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
@@ -8,9 +9,10 @@ var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
 var url = 'mongodb://localhost:27017/grid';
+var pidToLatLongList = getPidToLatLongList();
 
 module.exports = function() {
-
+    
     return new Promise(function(resolve, reject) {
         // Use connect method to connect to the Server
         MongoClient.connect(url, function (err, db) {
@@ -28,7 +30,7 @@ module.exports = function() {
                     if (err) {
                         console.log(err);
                     } else if (result.length) {
-                        //console.log('Found:', result);
+                        //console.log('Found:', result); 
                         for (var i=0; i < result.length; i++) {
                             var curr = result[i];
                             var address = curr['addresses[0]'];
@@ -37,7 +39,8 @@ module.exports = function() {
                             resArr.push({
                                 "address": addressStr,
                                 "kw": curr['kw'],
-                                "permitId": curr['permit id']
+                                "permitId": curr['permit id'],
+                                "geoLocation": pidToLatLongList[curr['permit id']]
                             });
                         }
                     } else {
